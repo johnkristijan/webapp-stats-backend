ee70d3a0f0bd260c786322289e17f2e092357141
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2022-10-31 13:22:05 +0100

    Storing thg PostgreSQL Pool and using it to avoid connect/disconnect every time a SQL query is initiated

--- sql.js
+++ sql.js
@@ -1,19 +1,22 @@
 // postgresql
 import postgresql from 'pg'
 
+const pool = new postgresql.Pool({
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+})
+
 const sqlQuery = async (SQL_QUERY) => {
-
-    const connection = new postgresql.Pool({
-        "host": process.env.PGHOST,
-        "database": process.env.PGDATABASE,
-        "port": process.env.PGPORT,
-        "user": process.env.PGUSER,
-        "password": process.env.PGPASSWORD
-    })
-
-    const response = connection.query(SQL_QUERY)
-    connection.end()
-    return response
+    try {
+        const res = await pool.query(SQL_QUERY)
+        return res
+    } catch (error) {
+        console.error(error)
+        return null
+    }
 }
 
 export default sqlQuery
