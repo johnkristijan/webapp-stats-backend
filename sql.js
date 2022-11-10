b3143a5854ee2f8da8426a751b52775fae13831d
John K Hermann <john.hermann@webstep.no>
2022-11-10 21:21:36 +0100

    backend service example updated

--- sql.js
+++ sql.js
@@ -2,13 +2,12 @@
 import postgresql from 'pg'
 
 const sqlQuery = async (SQL_QUERY) => {
-
     const connection = new postgresql.Pool({
-        "host": process.env.PGHOST,
-        "database": process.env.PGDATABASE,
-        "port": process.env.PGPORT,
-        "user": process.env.PGUSER,
-        "password": process.env.PGPASSWORD
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        port: process.env.PGPORT,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
     })
 
     const response = connection.query(SQL_QUERY)
